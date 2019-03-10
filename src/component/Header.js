ead705ffaac1c2b5e1cc02ae0bf840d21585b6ff
unknown <mrazzakov@gmail.com>
2019-03-10 00:44:43 -0600

    components interact with js code

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 function Header() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if(hours < 12) //12am-11:59am
+        timeOfDay = "morning";
+    else if(hours >= 12 && hours < 17) //12-5pm
+        timeOfDay = "afternoon";
+    else
+        timeOfDay = "night";
+
+        
     return (
-        <header className="navbar">This is the Navigation</header>
+        <div>
+            <header className="navbar">
+                Good {timeOfDay}!               
+            </header>
+        </div>
     );
 }
 
