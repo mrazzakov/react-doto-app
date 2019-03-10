7be1463c74fc039497818404c118b454b4c2cea1
unknown <mrazzakov@gmail.com>
2019-03-10 00:11:29 -0600

    added checkboxes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input type="checkbox"/>
+        <p>List Item 1</p>
+        <input type="checkbox"/>
+        <p>List Item 2</p>
+        <input type="checkbox"/>
+        <p>List Item 3</p>
       </div>
     );
   }
