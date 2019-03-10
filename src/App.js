9fe6db5fe89be99fbdfb2f3f29ddb8e482ef80f9
unknown <mrazzakov@gmail.com>
2019-03-10 00:22:35 -0600

    added header; styled header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './component/Header';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <input type="checkbox"/>
         <p>List Item 1</p>
         <input type="checkbox"/>
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="navbar">This is the Navigation</header>
+    );
+}
+
+export default Header;
